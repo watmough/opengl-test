c228f061d851f0bb207bacd190a244acee97d26b
Jonathan Watmough <jonathan.watmough@gmail.com>
2018-01-01 07:38:24 -0600

    freeglut demos

---
+++ hello-gl.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
+
+void renderScene(void) {
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+		glVertex3f(-0.5,-0.5,0.0);
+		glVertex3f(0.5,0.0,0.0);
+		glVertex3f(0.0,0.5,0.0);
+	glEnd();
+
+    glutWireTeapot(30);
+    std::cout << glutGet(GLUT_WINDOW_WIDTH) << " " << glutGet(GLUT_WINDOW_HEIGHT) << std::endl;
+
+        glutSwapBuffers();
+}
+
+int main(int argc, char **argv) {
+
+	// init GLUT and create Window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(320,320);
+	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+
+	// register callbacks
+	glutDisplayFunc(renderScene);
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
+	
+	return 1;
+}
